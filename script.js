54c847cd459f81cfe3f3617f84954b92d0a5cbcf
DmlaOzturk <dmla.ozturk@gmail.com>
2022-04-16 22:31:25 +0300

    added strike-through option

--- script.js
+++ script.js
@@ -1,66 +1,44 @@
-
 var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
 
-function inputLength(){
+
+function inputLength() {
 	return input.value.length;
 }
 
-function createListElement(){
-	var li= document.createElement("li");
-    li.appendChild(document.createTextNode(input.value));
-    ul.appendChild(li);
-    input.value ="";
-
+function createListElement() {
+	var li = document.createElement("li");
+	li.appendChild(document.createTextNode(input.value));
+	ul.appendChild(li);
+	input.value = "";
 }
 
-function addListAfterClick(){
-	if (inputLength() > 0){
-     createListElement();
-    }
+function addListAfterClick() {
+	if (inputLength() > 0) {
+		createListElement();
+	}
 }
 
-function addListAfterKeypress(event){
-     if (inputLength() > 0 && event.keyCode === 13){
-     createListElement();
-    }
+function addListAfterKeypress(event) {
+	if (inputLength() > 0 && event.keyCode === 13) {
+		createListElement();
+	}
 }
+// üstünü çizme kısmı
+
+var item = document.getElementsByClassName("fin");
+for(var i=0; i< item.length;i++){
+	item[i].addEventListener("click",toggle);
+}
+
+function toggle(){
+	this.classList.toggle("done");
+}
+
 
 button.addEventListener("click", addListAfterClick);
+
 input.addEventListener("keypress", addListAfterKeypress);
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*var button =document.getElementsByTagName("button")[0];
-
-button.addEventListener("click", function(){
-	console.log("Clicked!!!");
-});
-*/
